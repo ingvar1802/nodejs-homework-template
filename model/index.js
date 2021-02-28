39b06642c5b6d880b872355cc22d25bb9ad6ce0a
Ihor Nadieien <ingwar1802@gmail.com>
2021-02-28 11:25:17 +0200

    add app

--- model/index.js
+++ model/index.js
@@ -1,15 +1,44 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const db = require('./db')
+const { v4: uuid } = require('uuid')
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  return db.get('contacts').value()
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  return db
+    .get('contacts')
+    .find((contact) => String(contact.id) === contactId)
+    .value()
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const [record] = db
+    .get('contacts')
+    .remove((contact) => String(contact.id) === contactId)
+    .write()
+  return record
+}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const id = uuid()
+  const record = {
+    id,
+    ...body,
+  }
+  db.get('contacts').push(record).write()
+  return record
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const record = db
+    .get('contacts')
+    .find((contact) => String(contact.id) === contactId)
+    .assign(body)
+    .value()
+  db.write()
+  return record
+}
 
 module.exports = {
   listContacts,
