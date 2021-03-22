3f5246e8e8eb538aad1a725fdc593f42f420897f
Ihor Nadieien <63648254+ingvar1802@users.noreply.github.com>
2021-03-22 12:42:30 +0200

    add app

--- model/db.js
+++ model/db.js
@@ -1,30 +1,31 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
+require ('dotenv').config()
 const uriDb = process.env.URI_DB
 
 const db = mongoose.connect(uriDb, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify:false,
 })
 
 mongoose.connection.on('connected', () => {
-  console.log('Mongoose connection to db')
+    console.log('Database connection successful');
 })
 
 mongoose.connection.on('error', (err) => {
-  console.log(`Mongoose connection error: ${err.message}`)
+    console.log(`Mongoose connecton error: ${err.message}`)
 })
 
 mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose disconnected')
+    console.log('Mongoose disconnected')
 })
 
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await mongoose.connection.close()
-  console.log('Connection for db closed and app termination')
-  process.exit(1)
+  console.log('Collection for db is closed and app terminated')
+  process.exit(1);
+
 })
 
 module.exports = db
